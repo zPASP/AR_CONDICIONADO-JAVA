f1499ae9fde38b3874e3a44322e28c475b8cd50d
Pedro Alexandre Pinto <zpasp@live.com>
2019-08-27 05:01:10 -0300

    Implementaçãoo da classe

--- ArCondicionado.java
+++ ArCondicionado.java
@@ -9,20 +9,22 @@ public class ArCondicionado {
     public boolean compressor;
     public boolean ventilacao;
     public int temperaturaAmbiente;
+    public String local;
     /*ATRIBUTOS */
     
     /*CONSTRUTOR */
 
-    public ArCondicionado () {
-        marca = "";
-        modelo = "";
-        potencia = 0;
-        consumo = 0;
-        temperaturaControle = 17;
+    public ArCondicionado (String nMarca, String nModelo, int tPotencia, int kConsumo, String nLocal) {
+        marca = nMarca;
+        modelo = nModelo;
+        potencia = tPotencia;
+        consumo = kConsumo;
+        local = nLocal;
+        temperaturaControle = 23;
         ligado = false;
         compressor = false;
         ventilacao = false;
-        temperaturaAmbiente = 16;
+        temperaturaAmbiente = 0;
     }
 
     /*CONSTRUTOR */
@@ -44,60 +46,127 @@ public class ArCondicionado {
         ventilacao = true;
     }
 
+    public void ativaCompressor () {
+        compressor = true;
+        ventilacao = false;
+    }
+
     public void aumentaTemperaturaControle () {
-        if (temperaturaControle > 17 && temperaturaControle < 40){
+        if (temperaturaControle > 17 && temperaturaControle < 40){ // limito o controle a ficar entre as temperatura de 17 ate 40 graus
             temperaturaControle++;
         }
     }
 
     public void diminuiTemperaturaControle () {
-        if (temperaturaControle > 17 && temperaturaControle < 40) {
+        if (temperaturaControle > 17 && temperaturaControle < 40) { // limito o controle a ficar entre as temperatura de 17 ate 40 graus
             temperaturaControle--;
         }
     }
 
+    public void alterarTemperaturaControle (int tAlterar) {
+        temperaturaControle = tAlterar;
+    }
+
     public int aumentaTemperaturaAmbiente () {
         int temperaturaFinal; //crio a veriavel para retorno da temperatura ambiente
-        if (temperaturaAmbiente < temperaturaControle) { // comparo a temperatura ambiente com a temperatura escolhida para o programa saber o que fazer
-            compressor = true; // ligo o compressor
-            temperaturaAmbiente++; //compremento +1 na temperatura ambiente
-            temperaturaFinal = temperaturaAmbiente; //a variavel de retorno recebe o valor da temperatura ambiente
-        }else { // senão
-            ativaVentilacao(); //ativo a ventilaçao
-            temperaturaFinal = temperaturaAmbiente; //a variavel de retorno recebe o valor da temperatura ambiente
-        }
+        ativaCompressor(); // ligo o compressor
+        temperaturaAmbiente++; //compremento +1 na temperatura ambiente
+        temperaturaFinal = temperaturaAmbiente; // atribuo a temperatura final aumentada ah variavel de retorno
+        if ( temperaturaControle == temperaturaAmbiente ) 
+            ativaVentilacao();
+        else 
+            ativaCompressor() ; // tratamento para desligar o compressor no momento certo
         return temperaturaFinal;
     }
 
     public int diminuiTemperaturaAmbiente () {
         int temperaturaFinal;
-        if (temperaturaAmbiente > temperaturaControle) { // comparo a temperatura ambiente com a temperatura escolhida para o programa saber o que fazer
-            compressor = true; // ligo o compressor
-            temperaturaAmbiente--; //compremento -1 na temperatura ambiente
-            temperaturaFinal = temperaturaAmbiente; //a variavel de retorno recebe o valor da temperatura ambiente
+        ativaCompressor(); // ligo o compressor
+        temperaturaAmbiente--; //compremento -1 na temperatura ambiente
+        temperaturaFinal = temperaturaAmbiente; // atribuo a temperatura final diminuida ah variavel de retorno
+        if ( temperaturaControle == temperaturaAmbiente ) 
+            ativaVentilacao() ;
+        else 
+            ativaCompressor() ;
+        return temperaturaFinal;
+    }
+
+
+    public void modoAutomatico () {
+        int tempAtual = temperaturaAmbiente;
+        if (tempAtual < temperaturaControle){ // comparo a temperatura ambiente com a temperatura escolhida para o programa saber o que fazer
+            aumentaTemperaturaAmbiente(); //compremento +1 na temperatura ambiente
+        }else if (tempAtual > temperaturaControle) {
+            diminuiTemperaturaAmbiente(); //compremento -1 na temperatura ambiente
+        }else {
+            ativaVentilacao(); // ativo a ventilacao
+        }
+    }
+
+    public void modoVerao (){ // esse modo apenas aumenta a temperatura e/ou ventila o ambiente
+        int tempAtual = temperaturaAmbiente;
+        if (tempAtual < temperaturaControle){ // comparo a temperatura ambiente com a temperatura escolhida para o programa saber o que fazer
+            aumentaTemperaturaAmbiente(); //compremento +1 na temperatura ambiente
+        }else {
+            ativaVentilacao(); // ativo a ventilacao
+        }
+        
+    }
+
+    public void modoInverno () {
+        int tempAtual = temperaturaAmbiente;
+        if (tempAtual > temperaturaControle) { // comparo a temperatura ambiente com a temperatura escolhida para o programa saber o que fazer
+            diminuiTemperaturaAmbiente();
         }else { // senão
             ativaVentilacao(); //ativo a ventilaçao
-            temperaturaFinal = temperaturaAmbiente; //a variavel de retorno recebe o valor da temperatura ambiente
         }
-
-        return temperaturaFinal;
     }
 
     public void mudarTemperaturaAmbiente (int temperatura) {
         temperaturaAmbiente = temperatura;
     }
 
+    public int mostrarTemperaturaControle (){
+        int tempControle = temperaturaControle;
+        return tempControle;
+    }
+
+    public void estadoCompressor () {
+        System.out.println("\n COMPRESSOR :" + compressor);
+    }
+
+
+    public void mostrarInformacoes () {
+        System.out.println ( "");
+        System.out.println (  "\n Status do arcondicionado : " + (ligado ? "LIGADO" : "DESLIGADO"));
+        System.out.println (  "\n Compressor : " + (compressor ? "LIGADO" : "DESLIGADO")) ;
+        System.out.println (  "\n Ventilação : " + (ventilacao ? "LIGADO" : "DESLIGADO"));
+        System.out.println (  "\n -----------------------------------------");
+        System.out.println (  "\n Temparatura no Controle : " + temperaturaControle);
+        System.out.println (  "\n Temperatura Ambiente : " + temperaturaAmbiente);
+        System.out.println (  "\n -----------------------------------------");
+        
+    }
+
+
     public String toString () { // retorno ao chamar apenas a classe sem chamar metodo
         String retorna = "";
+        retorna += "\n -----------------------------------------";
+        retorna += "\n \tAR-CONDICIONADO : " + local;
+        retorna += "\n -----------------------------------------";
         retorna += "\n Marca : " + marca;
         retorna += "\n Modelo : " + modelo;
         retorna += "\n Potencia : " + potencia;
         retorna += "\n Consumo : " + consumo;
-        retorna += "\n Temparatura no Controle : " + temperaturaControle;
+        retorna += "\n -----------------------------------------";
         retorna += "\n Status do arcondicionado : " + (ligado ? "LIGADO" : "DESLIGADO");
         retorna += "\n Compressor : " + (compressor ? "LIGADO" : "DESLIGADO") ;
-        retorna += "\n Ventilação : " + (ventilacao ? "LIGADO" : "DESLIGADO");
-        retorna += "\n Temperatura Ambiebnte : " + temperaturaAmbiente;
+        retorna += "\n Ventilacao : " + (ventilacao ? "LIGADO" : "DESLIGADO");
+        retorna += "\n -----------------------------------------";
+        retorna += "\n Temparatura no Controle : " + temperaturaControle;
+        retorna += "\n Temperatura Ambiente : " + temperaturaAmbiente;
+        retorna += "\n -----------------------------------------";
+
 
         return retorna;
     }
--- Controle.java
+++ Controle.java
@@ -1,27 +1,190 @@
+import java.util.Scanner;
+
 public class Controle {
+
+    public static void telaBoasVindas(int repetindo) { //teça de abertura do sistema aparece somente na primeira repetição
+        if (repetindo == 0){ //defino que se estiver repedindo ou o valor for diferente de 0 não ira ser executado
+            System.out.println("+------ BEM VINDO A CASA AUTO-CRIMATIZADA -------+");
+            System.out.println("|------------------------------------------------|");
+            System.out.println("|------**LIGANDO TODOS OS AR-CONCIONADOS**-------|");
+            System.out.println("|                                                |");
+        }
+    }
+    
+    public static int trocarModoArCondionado () { // opção para escolher o modo operante do ar-condicionado
+
+        int retornaModo = 0;
+        int modoOperacao = -2;
+
+
+        System.out.println("|INFORME O MODO DE OPERACAO DO AR-CONDICIONADO:  |");
+        System.out.println("|1 - AUTOMATICO                                  |");
+        System.out.println("|2 - VERAO                                       |");
+        System.out.println("|3 - INVERNO                                     |");
+
+            while (modoOperacao < -1 || modoOperacao > 3) { //repetição para aceitar somente os valores validos
+                System.out.print("\t\t");
+                modoOperacao = Integer.parseInt(System.console().readLine());
+            if (modoOperacao > 3 || modoOperacao < 1 )
+                System.out.println("|   [ERROR] VALOR INVALIDO                       |");
+                if (modoOperacao == 0) modoOperacao = -2; // caso seja informado um valor 0 nesse menu não é para ele ser valido, então eu altero o valor para a repetição se manter
+            }
+        retornaModo = modoOperacao;
+
+        return retornaModo;
+    }
+
+    public static int escolherTrocarModoArCondionado(int repetindo,int modoEscolhido) { // saber se quer trocar o modo de opeção do ar
+        int escolhaRetorno = 0;
+        int escolhaRepeti = 3;
+
+            if (repetindo != 0) { //não é valido para a primeira execução
+                System.out.println("|  TROCA O MODO DE OPERACAO DO AR-CONDICIONADO   |");
+                System.out.println("|1 - SIM                                         |");
+                System.out.println("|0 - NAO                                         |");
+                    
+                    while (escolhaRepeti < -1 || escolhaRepeti > 1) { //repetição para aceitar somente valores validos
+                        System.out.print("\t\t");
+                        escolhaRepeti = Integer.parseInt(System.console().readLine());
+                        if (escolhaRepeti > 1 || escolhaRepeti < 0 )
+                            System.out.println("|   [ERROR] VALOR INVALIDO                       |");
+                    }
+
+                    if (escolhaRepeti == 1){ //se valor 1
+                            escolhaRetorno = trocarModoArCondionado(); //chamo a função para escolher o modo do ar novamente
+                    }else if (escolhaRepeti == 0){ //se valor 0
+                        System.out.println("|        MODO DO AR-CONDIONADO NAO ALTERADO      |"); //mantenho o modo que ja estava
+                        escolhaRetorno = modoEscolhido;
+                    }else if (escolhaRepeti == -1){ //se valor -1 não faço nada e encaminha para a temperatura do controle
+                        escolhaRetorno = -1;
+                    }
+
+            }else { // senão
+                escolhaRetorno = trocarModoArCondionado(); //se for a primeira vez encaminho direto sem perguntar nada
+            }
+        return escolhaRetorno;
+    }
+
+    public static int mudarTemperaturaControle(int tempControl) {
+        int tempFinal = 0;
+        
+        System.out.println("|------------------------------------------------|");
+        System.out.println("|          TEMPERATURA ATUAL DO CONTROLE         |");
+        System.out.println("|     " + tempControl + "                                         |");
+        System.out.println("|          ESCOLHA A TEMPERATURA DO CONTROLE     |");
+
+            while (tempFinal < 16 || tempFinal > 40 ) {//valor do controle entre 17 e 40 16 ficou para sair do programa
+                System.out.print("\t\t");
+                tempFinal = Integer.parseInt(System.console().readLine());
+                if (tempFinal == 16) tempFinal = 10; // caso seja digitado 16 transformo em outro valor não valido diferente de -1
+                if (tempFinal == -1) tempFinal = 16; //caso seja -1 transformo em 16 para poder sair da repetição feita
+
+            if (tempFinal > 40 || tempFinal < 17  && tempFinal != -1)
+                System.out.println("|   [ERROR] VALOR INVALIDO                       |");
+            }
+            if (tempFinal == 16) tempFinal = -1; // tratamento do valor 16 tranformando em -1 novamente para sair do programa
+
+        System.out.println("|------------------------------------------------|");
+
+        return tempFinal;
+    }
+
     public static void main(String[] args) {
-        ArCondicionado sala;
+        
+        Scanner in = new Scanner(System.in);
+        
+        ArCondicionado sala,escritorio,quarto;
+        /**
+         * METODOS TESTADOS
+         * temperatura ambiente não ultrapassa o valor da temperatura do controle
+         * temperatura controle não ultrapassa os valores de 17 até 40
+         * toString mostrando corretamente as insformações da classe
+         * 
+         */
 
-        sala = new ArCondicionado();
 
+        /* PROCESSAMENTO INTERNO SEM INTERRAÇÂO DO USER */
+            /*ATRIBUIçÃO DAS CLASSES */
+        escritorio = new ArCondicionado("Eletrolux", "ArtCool", 9000, 638,"Escritorio");
+        sala = new ArCondicionado("Philco", "EcoTurbo", 18000, 5200,"Sala");
+        quarto = new ArCondicionado("Samsung", "TurboMaster", 1200, 3510,"Quarto");
+            /**LIGAR OS ARES */
+        escritorio.ligaArCondicionado();
         sala.ligaArCondicionado();
-
+        quarto.ligaArCondicionado();
+            /*VALORES ALEATORIO PARA A TEMPERATURA AMBIENTE. DE 0 Até 40 */
+        escritorio.mudarTemperaturaAmbiente((int) (Math.random()*40+1));
+        sala.mudarTemperaturaAmbiente((int) (Math.random()*40+1));
+        quarto.mudarTemperaturaAmbiente((int) (Math.random()*40+1));
+            
+        System.out.println(escritorio);
         System.out.println(sala);
+        System.out.println(quarto);
 
-        sala.aumentaTemperaturaAmbiente();
-        sala.aumentaTemperaturaAmbiente();
-        sala.aumentaTemperaturaAmbiente();
-        sala.aumentaTemperaturaAmbiente();
-        sala.diminuiTemperaturaControle();
+        /** */
 
-        System.out.println(sala);
+        boolean continuarPrograma = true;
 
-        sala.mudarTemperaturaAmbiente(40);;
-        sala.aumentaTemperaturaAmbiente();
-        sala.aumentaTemperaturaAmbiente();
-        sala.aumentaTemperaturaAmbiente();
+        int interacao = 0;
+        int contador = 0;
 
-        System.out.println(sala);
+        int modoOperacao = 0;
+        int valorTemperatura = sala.mostrarTemperaturaControle();
 
+
+        while(continuarPrograma) {
+            
+                telaBoasVindas(interacao);
+            
+                modoOperacao = escolherTrocarModoArCondionado(interacao, modoOperacao);
+
+                valorTemperatura = mudarTemperaturaControle(valorTemperatura);
+                
+                escritorio.alterarTemperaturaControle(valorTemperatura);
+                sala.alterarTemperaturaControle(valorTemperatura);
+                quarto.alterarTemperaturaControle(valorTemperatura);
+
+                System.out.println("TEMPERATURA CONTROLE: "+ valorTemperatura);
+                System.out.println("MODO DE ESCOLHA: "+modoOperacao);
+
+                if (valorTemperatura == -1 || modoOperacao == -1){
+                    System.out.println("|------------------------------------------------|");
+                    System.out.println("|   FECHANDO O PROGRAMA CASA AUTO_CLIMATIZADA    |");
+                    System.out.println("|-----**DESLIGANDO TODOS OS AR-CONCIONADOS**-----|");
+                    System.out.println("|------------------------------------------------|");
+                    continuarPrograma = false;
+                    escritorio.desligaArCondicionado();
+                    sala.desligaArCondicionado();
+                    quarto.desligaArCondicionado();
+
+                }
+                
+                escritorio.mostrarInformacoes();
+                System.out.println("10x DEPOIS");
+
+                while (contador < 10  && continuarPrograma) {
+                  //  System.out.println("INTERACAO "+contador+":");
+                    if (modoOperacao == 1){ //automatico
+                        escritorio.modoAutomatico();
+                        sala.modoAutomatico();
+                        quarto.modoAutomatico();
+                        
+                    }else if(modoOperacao == 2){ // verão
+                        escritorio.modoVerao();
+                        sala.modoVerao();
+                        quarto.modoVerao();
+
+                    }else { //inverno
+                        escritorio.modoInverno();
+                        sala.modoInverno();
+                        sala.modoInverno();
+
+                    }
+                    contador++;
+                }
+                escritorio.mostrarInformacoes();
+            contador=0;
+            interacao++;
+        }
     }
 }
